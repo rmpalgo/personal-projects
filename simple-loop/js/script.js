9b64ef0fedf53533ef9cd462849b553162fa7420
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-21 12:45:03 -0500

    added a for loop and it shot the yellow ball to infinity to the right.

--- simple-loop/js/script.js
+++ simple-loop/js/script.js
@@ -13,21 +13,24 @@ function myMove() {
 
 //    This is the function that moves the target animation element
     function frame() {
-        if (pos == 580) {
-            //    when it hits the edge at 850px then clear for now, will
-            //    change it later to turn back, and see how I can incorporate loops
-            // clearInterval(id);
-        // instead of clearing to stop why not make it go backwards?
-            pos++;
-            target.style.top = 0;
-            target.style.right = pos + 'px';
-        } else {
-            //add 1 to pos = 0
-            pos++;
-            //keep top at 0, and only move pos x-axis
-            target.style.top = 0;
-            target.style.left = pos + 'px';
+        for (var i =0; i < 100; i++) {
+            if (pos == 580) {
+                //    when it hits the edge at 850px then clear for now, will
+                //    change it later to turn back, and see how I can incorporate loops
+                // clearInterval(id);
+                // instead of clearing to stop why not make it go backwards?
+                pos++;
+                target.style.top = 0;
+                target.style.right = pos + 'px';
+            } else {
+                //add 1 to pos = 0
+                pos++;
+                //keep top at 0, and only move pos x-axis
+                target.style.top = 0;
+                target.style.left = pos + 'px';
 
+            }
         }
+
     }
 }
