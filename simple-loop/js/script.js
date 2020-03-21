f0d2cde6035379409445989cb081544c9729d131
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-21 13:19:55 -0500

    If change sign to incremenntal pixels, you can make the ball go backwards by using variable for the change, however when the ball goes to infinity reverse.

--- simple-loop/js/script.js
+++ simple-loop/js/script.js
@@ -6,31 +6,29 @@
 function myMove() {
 //    we target the animation yellow circle with getElementById
     var target = document.getElementById("animation");
-    var pos = 0;
     //this will initiate the function frame every second value.
     //the setInterval acts like a loop?...
     var id = setInterval(frame, 10);
-
+    var pos = 0;
+    var incr = 1;
 //    This is the function that moves the target animation element
     function frame() {
-        for (var i =0; i < 100; i++) {
-            if (pos == 580) {
+            if (pos >= 580) {
                 //    when it hits the edge at 850px then clear for now, will
                 //    change it later to turn back, and see how I can incorporate loops
                 // clearInterval(id);
                 // instead of clearing to stop why not make it go backwards?
-                pos++;
-                target.style.top = 0;
-                target.style.right = pos + 'px';
+                //found solution change the sign of the increments
+                incr = -1;
             } else {
                 //add 1 to pos = 0
-                pos++;
                 //keep top at 0, and only move pos x-axis
                 target.style.top = 0;
                 target.style.left = pos + 'px';
-
             }
-        }
-
+            //by using an incr variable you can just change
+            //the sign to negative to make it go backwards when
+            //it hits the edge of the box
+            pos = pos + incr;
     }
 }
