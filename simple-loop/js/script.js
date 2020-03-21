eee4f0501b4cbf87e1287e5790563746096fe4fb
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-21 12:37:01 -0500

    first try of making the yellow circle to go back, it did not work.

--- simple-loop/js/script.js
+++ simple-loop/js/script.js
@@ -8,26 +8,26 @@ function myMove() {
     var target = document.getElementById("animation");
     var pos = 0;
     //this will initiate the function frame every second value.
+    //the setInterval acts like a loop?...
     var id = setInterval(frame, 10);
+
 //    This is the function that moves the target animation element
     function frame() {
-        if (pos == 850) {
-        //    when it hits the edge at 850px then clear for now, will
-        //    change it later to turn back, and see how I can incorporate loops
-            clearInterval(id);
+        if (pos == 580) {
+            //    when it hits the edge at 850px then clear for now, will
+            //    change it later to turn back, and see how I can incorporate loops
+            // clearInterval(id);
+        // instead of clearing to stop why not make it go backwards?
+            pos++;
+            target.style.top = 0;
+            target.style.right = pos + 'px';
         } else {
             //add 1 to pos = 0
             pos++;
-            //add
-            elem.style.top = pos + 'px';
+            //keep top at 0, and only move pos x-axis
+            target.style.top = 0;
+            target.style.left = pos + 'px';
 
         }
     }
-
-
-
-
-
-
-
 }
