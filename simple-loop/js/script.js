6048af5ba71992fdc5ca88a36da6a80b101ced2a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-21 16:15:23 -0500

    Made the ball move back both in the x and y axis.

--- simple-loop/js/script.js
+++ simple-loop/js/script.js
@@ -11,7 +11,9 @@ function myMove() {
     var id = setInterval(frame, 10);
     var xpos = 0;
     var ypos = 0;
-    var incr = 2;
+    var xincr = 2;
+    var yincr = 2;
+
     //trying to randomize the top direction;
     var random = function () {
            return (Math.random() * .01);
@@ -26,22 +28,25 @@ function myMove() {
                 // clearInterval(id);
                 // instead of clearing to stop why not make it go backwards?
                 //found solution change the sign of the increments
-                incr = -2;
+                xincr = -2;
+                yincr = -2
             } else {
                 //add 1 to pos = 0
                 //keep top at 0, and only move pos x-axis
-                target.style.top = ypos + 'px';
+                target.style.top =  ypos + 'px';
                 target.style.left = xpos + 'px';
             }
             //by using an incr variable you can just change
             //the sign to negative to make it go backwards when
             //it hits the edge of the box
-            xpos = xpos + incr;
-            ypos = ypos + 1;
+            xpos = xpos + xincr;
+            ypos = ypos  + yincr;
             //so we if we a negative value we can use this as an indicator to
             //go back now I have a working game that goes back and forth woot!
+
             if (xpos == -6) {
-                incr = +2;
+                xincr = +2;
+                yincr = -2;
             }
     }
 }
