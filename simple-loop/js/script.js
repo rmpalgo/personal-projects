9ca1307970439b23880f015184cbce290a6b0c7f
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-21 19:00:01 -0500

    from changing directions ball branch

--- simple-loop/js/script.js
+++ simple-loop/js/script.js
@@ -9,36 +9,6 @@ function myMove() {
     //this will initiate the function frame every second value.
     //the setInterval acts like a loop?...
     var id = setInterval(frame, 10);
-<<<<<<< HEAD
-    var pos = 0;
-    var incr = 1;
-    //add a random denom to change the velocity
-    var randomDenom = (Math.random() * 3);
-//    This is the function that moves the target animation element
-    function frame() {
-            if (pos >= 580) {
-                //    when it hits the edge at 850px then clear for now, will
-                //    change it later to turn back, and see how I can incorporate loops
-                // clearInterval(id);
-                // instead of clearing to stop why not make it go backwards?
-                //found solution change the sign of the increments
-                incr = -1;
-            } else {
-                //add 1 to pos = 0
-                //keep top at 0, and only move pos x-axis
-                target.style.top = pos/randomDenom + 'px';
-                target.style.left = pos + 'px';
-            }
-            //by using an incr variable you can just change
-            //the sign to negative to make it go backwards when
-            //it hits the edge of the box
-            pos = pos + incr;
-            //so we if we a negative value we can use this as an indicator to
-            //go back now I have a working game that goes back and forth woot!
-            if (pos == -2) {
-                incr = +1;
-            }
-=======
     var xpos = 0;
     var ypos = 0;
     var xincr = 2;
@@ -46,7 +16,7 @@ function myMove() {
 
     //trying to randomize the top direction;
     var random = function () {
-           return (Math.random() * .01);
+        return (Math.random() * .01);
     }
 
 
@@ -78,6 +48,5 @@ function myMove() {
             xincr = 2;
             yincr = 1.5 * xincr;
         }
->>>>>>> changing-directions-ball
     }
 }
