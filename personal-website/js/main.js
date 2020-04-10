9a6400258b613178853906831687a989b1852d46
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-10 08:29:14 -0500

    Added sliding animation to hello

---
+++ personal-website/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready( function() {
+ "use strict";
+
+ $(window).on('load', function () {
+
+     $('.hello').animate({
+         left: '345px',
+         opacity: '1'
+     }, 1500);
+ });
+
+
+
+});
