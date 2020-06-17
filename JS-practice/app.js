99b0762809671331901b161462d7f247e2e7e70c
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-16 21:50:53 -0500

    feat: rest and spread.

--- JS-practice/app.js
+++ JS-practice/app.js
@@ -22,4 +22,23 @@ p.printMyName();
 p.printGender();
 
 
+//Spread Operator
+const oldArray = [1, 2, 3];
+const newArray = [...oldArray, 4, 5, 6];
+console.log(newArray);
 
+const oldObj = {
+    fieldOne: 1,
+    fieldTwo: 2,
+    fieldThree: 3
+}
+
+const newObj = {...oldObj, fieldFour: 4};
+console.log(newObj);
+
+//Rest Operator
+const filter = (...args) => {
+    return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3, 1, 2, 2, 1));
